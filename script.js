fca67f965f3a3af5ecdfcc76840968873137c7ee
Plokhuta <dimaploxyta@gmail.com>
2019-07-10 19:20:52 +0300

    comply task 3

--- script.js
+++ script.js
@@ -22,7 +22,8 @@ var appData = {
     expenses: {},
     optionalExpenses: {},
     income: [],
-    savings: true
+    savings: true,
+    optionalExpenses: {}
 };
 
 
@@ -48,20 +49,26 @@ chooseExpenses();
 
 
 
-
-appData.moneyPerDay = (appData.budjet / 30).toFixed(2);
-alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "руб.");
-
-
-if (appData.moneyPerDay < 100) {
-    console.log ("Это минимальный уровень достатка!");
-} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
-    console.log ("Это средний уровень достатка!");
-} else if (appData.moneyPerDay > 2000) {
-    console.log ("Это высокий уровень достатка!");
-} else {
-    console.log ("Произошла ошибка");
+function detectDayBudget(){
+    appData.moneyPerDay = (appData.budjet / 30).toFixed(2);
+    alert("Бюджет на 1 день составляет " + appData.moneyPerDay + "грн.");  
 }
+
+detectDayBudget();
+
+
+function detectLevel(){
+    if (appData.moneyPerDay < 100) {
+        console.log ("Это минимальный уровень достатка!");
+    } else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+        console.log ("Это средний уровень достатка!");
+    } else if (appData.moneyPerDay > 2000) {
+        console.log ("Это высокий уровень достатка!");
+    } else {
+        console.log ("Произошла ошибка");
+    }
+}
+
 
 function checkSavings(){
     if(appData.savings == true){
@@ -74,3 +81,13 @@ function checkSavings(){
 }
 
 checkSavings();
+
+function chooseOptExpenses(){
+    for(let i = 1; i <= 3; i++){
+        let questionOptExpenses = prompt("Статья необязательных расходов?");
+        appData.optionalExpenses[i] = questionOptExpenses;
+    }
+    console.log(appData.optionalExpenses);
+}
+
+chooseOptExpenses();

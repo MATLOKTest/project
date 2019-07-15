d5de98d983728e2e460c58fd01daf40081da0e76
Plokhuta <dimaploxyta@gmail.com>
2019-07-16 00:40:45 +0300

    pre task 7

---
+++ Завдання_7/js/main.js
@@ -0,0 +1,4 @@
+let buttonStart = document.getElementById("start"),
+    budgetValue = document.getElementsByClassName("budget-value"),
+    daybudgetValue = document.getElementsByClassName("daybudget-value"),
+    levelValue = document.getElementsByClassName("level-value"),
